1f4c5cac0323f7ae097bab833168b448b15204ad
Nikolay <merzlovnik@mail.ru>
2020-04-04 11:00:22 +0300

    added name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-  cout<<"Hello world"<<endl;
+  string name;
+
+  cout<<"enter your name"<<endl;
+  cin>>name;
+
+  cout<<"Hello world from "<<name<<endl;
 
   return 0;
 }
